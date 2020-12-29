fd5a46f53e576b892ed4dafc8c0ab1bb22a79832
lukewei <weikelujava@163.com>
2020-12-29 09:46:14 +0800

    SpringBoot整合ES Mybatis-plus

--- spring-cloud-alibaba/c-cloud-business/c-search-center/src/main/java/com/smart/search/service/ISuperService.java
+++ spring-cloud-alibaba/c-cloud-business/c-search-center/src/main/java/com/smart/search/service/ISuperService.java
@@ -2,16 +2,6 @@ package com.smart.search.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 
-/**
- * All rights Reserved, Designed By http://www.hollysmart.com.cn
- *
- * @version V1.0.0
- * @title: ISuperService
- * @description: service接口父类
- * @author: hollysmart
- * @date: 2019/10/16 12:31
- * @remark:
- */
 public interface ISuperService<T> extends IService<T> {
 
 }
