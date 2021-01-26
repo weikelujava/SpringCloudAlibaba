216d8cbed0a7b1223f43e0232bbf0ebef80b5097
lukewei <weikelujava@163.com>
2021-01-26 13:07:49 +0800

    增加Spring源码流程测试

---
+++ spring-cloud-alibaba/c-cloud-business/c-demo-center/src/main/java/com/smart/demo/spring/SpringTest.java
@@ -0,0 +1,22 @@
+package com.smart.demo.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @version V1.0
+ * @title: SpringTest
+ * @description:
+ * @author: lukewei
+ * @date: 2021-01-26 9:47
+ * @remark: 修改内容
+ */
+public class SpringTest {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+        User user = context.getBean("user", User.class);
+        System.out.println("name："+user.getName());
+    }
+}
---
+++ spring-cloud-alibaba/c-cloud-business/c-demo-center/src/main/java/com/smart/demo/spring/User.java
@@ -0,0 +1,23 @@
+package com.smart.demo.spring;
+
+/**
+ *
+ * @version V1.0
+ * @title: User
+ * @description:
+ * @author: lukewei
+ * @date: 2021-01-26 9:50
+ * @remark: 修改内容
+ */
+public class User {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
